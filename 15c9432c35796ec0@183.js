48da88e9fea7026588358b731f1c4f75cc33acf9
mrsnider123 <95854616+mrsnider123@users.noreply.github.com>
2021-12-15 14:01:06 +0100

    Add files via upload

---
+++ 15c9432c35796ec0@183.js
@@ -0,0 +1,101 @@
+// https://observablehq.com/@d3/force-directed-tree@183
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  const fileAttachments = new Map([["flare-2.json",new URL("./files/e65374209781891f37dea1e7a6e1c5e020a3009b8aedf113b4c80942018887a1176ad4945cf14444603ff91d3da371b3b0d72419fa8d2ee0f6e815732475d5de",import.meta.url)]]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], function(md){return(
+md`# Force-Directed Tree
+
+A [force-directed layout](/@d3/force-directed-graph) of a tree using [*hierarchy*.links](https://github.com/d3/d3-hierarchy/blob/master/README.md#node_links).`
+)});
+  main.variable(observer("chart")).define("chart", ["d3","data","width","height","drag","invalidation"], function(d3,data,width,height,drag,invalidation)
+{
+  const root = d3.hierarchy(data);
+  const links = root.links();
+  const nodes = root.descendants();
+
+  const simulation = d3.forceSimulation(nodes)
+      .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
+      .force("charge", d3.forceManyBody().strength(-50))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
+
+  const svg = d3.create("svg")
+      .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+  const link = svg.append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(links)
+    .join("line");
+
+  const node = svg.append("g")
+      .attr("fill", "#fff")
+      .attr("stroke", "#000")
+      .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+      .attr("fill", d => d.children ? null : "#000")
+      .attr("stroke", d => d.children ? null : "#fff")
+      .attr("r", 3.5)
+      .call(drag(simulation));
+
+  node.append("title")
+      .text(d => d.data.name);
+
+  simulation.on("tick", () => {
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+
+    node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+  });
+
+  invalidation.then(() => simulation.stop());
+
+  return svg.node();
+}
+);
+  main.variable(observer("data")).define("data", ["FileAttachment"], function(FileAttachment){return(
+FileAttachment("flare-2.json").json()
+)});
+  main.variable(observer("height")).define("height", function(){return(
+600
+)});
+  main.variable(observer("drag")).define("drag", ["d3"], function(d3){return(
+simulation => {
+  
+  function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+  
+  function dragged(event, d) {
+    d.fx = event.x;
+    d.fy = event.y;
+  }
+  
+  function dragended(event, d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+  
+  return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+}
+)});
+  main.variable(observer("d3")).define("d3", ["require"], function(require){return(
+require("d3@6")
+)});
+  return main;
+}
